35e60a3fa4c63851433a8f6bf9e6e4701ef742cb
bhunji2 <bhunji2@gmail.com>
2016-12-28 04:04:20 +0800

    2016.12.28 v1.61.0.4

--- SubFunction/GodBlessYou.lua
+++ SubFunction/GodBlessYou.lua
@@ -7,8 +7,13 @@ function GameStateMachine:change_state(state, params)
 	_GameStateMachine_change_state(self,state, params) 
 	--showS(self._current_state:name())
 	--showS("state change:"..state._name)
+	--ingame_fatal			未知：??
 	
 	if 	NoArrested  ~= true then return end
+	if 	state._name == "ingame_arrested"		or 		-- 逮捕上銬
+		state._name == "ingame_bleed_out" 		or 		-- 彌留之際
+		state._name == "ingame_electrified" 	or 		-- 電擊痙攣
+		state._name == "ingame_incapacitated" 	then 	-- 踢倒在地
 			GodBlessYou()
 	end
 end
